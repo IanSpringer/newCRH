6bdeb827585209d9b0ebf4e05dc882e4742fa312
Robert Springer <springerdrums@gmail.com>
2017-04-04 12:03:30 -0700

    85% complete

--- wp-content/themes/medical-clinic/js/custom.js
+++ wp-content/themes/medical-clinic/js/custom.js
@@ -7,14 +7,6 @@ $(document).ready(function(){
   $('.home .custom_html').on("mouseleave", function(){
     $(this).children('a').removeClass("fade-in")
   })
-  // $(window).scroll(function() {
-  //   $('#cmsmasters_row_zk7zqjv8n9 .home #cmsmasters_icon_list_items_ .cmsmasters_icon_list_icon').each(function(){
-  //   var imagePos = $(this).offset().top;
 
-  //   var halfWay = $(document).height()/2;
-  //       if (imagePos < halfWay) {
-  //           $('#cmsmasters_row_zk7zqjv8n9 .home #cmsmasters_icon_list_items_ .cmsmasters_icon_list_icon').addClass("icon-fade");
-  //       }
-  //   });
-  // });
+  $('#cmsmasters_row_n987711hsk .cmsmasters_toggle_wrap').first().addClass('current_toggle');
 })
