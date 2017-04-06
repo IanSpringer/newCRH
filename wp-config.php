fa55cf53424d8cbcc9f203879b3b611f7209f77d
Robert Springer <springerdrums@gmail.com>
2017-04-06 09:49:49 -0700

    changed db settings in config

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'crh-new');
+define('DB_NAME', 'slicedbr_crh');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'slicedbr_crh');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'T0a$ted0216!');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
